a520dd2b201160d36979bcd85700195bb86e001a
meisme-dev <omar.elantil@gmail.com>
2023-12-02 20:56:34 +0200

    flake test 8

--- packages/style/stylix.nix
+++ packages/style/stylix.nix
@@ -1,7 +1,14 @@
 { libs, config, pkgs, stylix, ... }:
 
 {
+    stylix.image = ../../assets/bg.png;
     stylix.polarity = "dark";
     stylix.autoEnable = true;
     stylix.base16Scheme = ./scheme.yaml;
+    stylix.opacity.applications = 0.75;
+    stylix.opacity.terminal = 0.75;
+    stylix.opacity.desktop = 0.75;
+    stylix.opacity.popups = 0.75;
+    stylix.fonts.sizes.applications = 10;
+    stylix.targets.kitty.variant256Colors = true;
 }
