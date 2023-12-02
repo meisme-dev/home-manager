532c762ed282ba9cbed258c52cd0aa6d476dab7a
meisme-dev <omar.elantil@gmail.com>
2023-12-02 19:26:49 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    stylix.url = "github:danth/stylix";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    home-manager.url = github:nix-community/home-manager;
+    stylix.url = github:danth/stylix;
   };
 
   outputs = { nixpkgs, home-manager, stylix, ... }: {
     homeConfigurations."meisme" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs.legacyPackages;
       modules = [ stylix.homeManagerModules.stylix ./home.nix ];
     };
   };
